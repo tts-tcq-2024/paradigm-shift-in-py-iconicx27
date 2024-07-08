cfda753f87b3e531603da38319f83c0a830b58b9
iconicx27 <77856027+iconicx27@users.noreply.github.com>
2024-07-08 10:00:41 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -4,10 +4,16 @@ def check_in_range(value, min_val, max_val, error_message):
         return False
     return True
 
+def is_charge_rate_ok(charge_rate):
+    if charge_rate > 0.8:
+        print('Charge rate is out of range!')
+        return False
+    return True
+
 def battery_is_ok(temperature, soc, charge_rate):
     return (check_in_range(temperature, 0, 45, 'Temperature is out of range!') and
             check_in_range(soc, 20, 80, 'State of Charge is out of range!') and
-            check_in_range(charge_rate, 0, 0.8, 'Charge rate is out of range!'))
+            is_charge_rate_ok(charge_rate)'))
 
 if __name__ == '__main__':
     assert(battery_is_ok(25, 70, 0.7) is True)
