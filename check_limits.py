70acea7fc3c44b0713d44519a9877d47e7dc8b30
iconicx27 <77856027+iconicx27@users.noreply.github.com>
2024-08-12 14:50:45 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -16,7 +16,12 @@ def check_parameter(value, min_val, max_val, parameter_name):
     return True
 
 def check_charge_rate(charge_rate):
-    return check_parameter(charge_rate, 0, 0.8, 'Charge rate')
+    if charge_rate > 0.8:
+        print('Charge rate is out of range!')
+        return False
+    elif charge_rate > 0.8 * 0.95:
+        print('Warning: Charge rate approaching upper limit!')
+    return True
 
 def battery_is_ok(temperature, soc, charge_rate):
     return (check_parameter(temperature, 0, 45, 'Temperature') and
@@ -26,5 +31,3 @@ def battery_is_ok(temperature, soc, charge_rate):
 if __name__ == '__main__':
     assert(battery_is_ok(25, 70, 0.7) is True)
     assert(battery_is_ok(50, 85, 0) is False)
-    assert(battery_is_ok(42, 78, 0.78) is True)
-    assert(battery_is_ok(10, 22, 0.79) is True)
